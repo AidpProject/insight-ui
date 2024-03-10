1986c9bf33d7b4d03d98f6564837647a4712345f
metavatem <loloadsl@hotmail.fr>
2024-03-10 22:27:02 +0000

    Update for Aidp

--- public/src/js/controllers/header.js
+++ public/src/js/controllers/header.js
@@ -6,8 +6,8 @@ angular.module('insight.system').controller('HeaderController',
 
     $rootScope.currency = {
       factor: 1,
-      symbol: 'RVN',
-      realSymbol: 'RVN'
+      symbol: 'AIDP',
+      realSymbol: 'AIDP'
     };
 
     $scope.menu = [{
